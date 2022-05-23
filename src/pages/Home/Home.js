e6ffe12ffdd79ef5c4bfa0c2d45c18c4efed8efd
Abdul <abdulmomin956@gmail.com>
2022-05-23 16:07:59 +0600

    tailwind and daisyui setup

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is home page</h1>
+            <h1 className='text-2xl font-bold text-primary'>This is home page</h1>
+            <button className='btn btn-secondary'> hello button</button>
         </div>
     );
 };
