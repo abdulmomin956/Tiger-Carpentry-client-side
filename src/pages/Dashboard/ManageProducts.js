3c1d5c2b679f5c1dfd08bcba4aa54532b3fd3289
Abdul <abdulmomin956@gmail.com>
2022-05-27 13:02:03 +0600

    portfolio half done

--- src/pages/Dashboard/ManageProducts.js
+++ src/pages/Dashboard/ManageProducts.js
@@ -1,9 +1,83 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import LoadSpinner from '../shared/LoadSpinner';
 
 const ManageProducts = () => {
+    const allProducts = useQuery('allOrder', () => fetch('http://localhost:5000/all-products', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (allProducts.isLoading) {
+        return <div className='h-screen flex justify-center items-center'>
+            <LoadSpinner></LoadSpinner>
+        </div>
+    }
+
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/all-products/${id}`, {
+            method: "DELETE",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(result => allProducts.refetch())
+    }
+
     return (
-        <div>
-            <h1>Manage Products</h1>
+        <div className="overflow-x-auto w-full border">
+            <div>
+                <h1 className=' text-4xl text-center font-bold uppercase'>Manage Products</h1>
+            </div>
+            <table className="table table-compact w-full">
+                <thead>
+                    <tr>
+                        <th>S/L</th>
+                        <th>Image</th>
+                        <th className='w-3'>Product</th>
+                        <th>Unit Price</th>
+                        <th>Available</th>
+                        <th>Minimum Order</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        allProducts.data.map((p, index) =>
+
+                            <tr key={index}>
+                                <th>{index + 1}</th>
+                                <td><img className='h-7' src={p.image} alt="" /></td>
+                                <th>{p.name} </th>
+                                <th>{p.price}$</th>
+                                <th>{p.availableQty} pcs</th>
+                                <th>{p.minOrder} pcs</th>
+                                <th><label htmlFor="my-modal-3" className="btn modal-button">Delete</label></th>
+
+                                <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                                <div className="modal">
+                                    <div className="modal-box relative">
+                                        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                        <h3 className="text-lg font-bold">Are you sure want to delete {p.name}?</h3>
+                                        <p className="py-4">If you delete, it will remove from database parmanently!</p>
+                                        <div className="modal-action">
+                                            <label onClick={() => handleDelete(p._id)} htmlFor="my-modal-3" className='btn bg-red-500 text-white'>Delete</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </tr>
+
+
+                        )
+                    }
+                </tbody>
+            </table>
+
+
         </div>
     );
 };
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,8 +2,226 @@ import React from 'react';
 
 const Portfolio = () => {
     return (
-        <div>
-            <h1>This is My Portfolio</h1>
+        <div className='py-4 px-40'>
+            <h1 className='text-4xl font-bold'>ABDUL MOMIN</h1>
+            <p>Address: Kamarpara, p/o# Shernagar, (6740), Belkuchi, Belkuchi, Sirajganj 6740</p>
+            <p>Primary Mobile No: 01890958782</p>
+            <p>Primary Email : abdulmomin956@gmail.com</p>
+            <p className='bg-base-100 underline mt-4 font-bold'>Academic Qualification:</p>
+
+            <table border="0" cellPadding="0" className='mt-2 border border-slate-400 border-collapse table-compact w-full' cellSpacing="0" align="center" width="100%" >
+                <tbody><tr className="border">
+                    <td className="border border-black" width="20%" align="center"  ><strong>Exam Title</strong></td>
+                    <td className="border border-black" width="20%" align="center" ><strong>Concentration/Major</strong></td>
+                    <td className="border border-black" width="20%" align="center" ><strong>Institute</strong></td>
+                    <td className="border border-black" width="12.5%" align="center" ><strong>Result</strong></td>
+
+                    <td className="border border-black" width="12.5%" align="center" ><strong>Pas.Year</strong></td>
+
+
+                    <td className="border border-black" width="15%" align="center"><strong>Duration</strong></td>
+
+                </tr>
+
+                    <tr className="border">
+
+                        <td className="border border-black" width="20%" align="center" >
+                            Bachelor of Science (BSc)
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center" >
+                            Physics
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Sirajganj Govt. College
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="12.5%" align="center"   >
+
+                            &nbsp;
+                        </td>
+
+
+                        <td className="border border-black" width="12.5%" align="center">
+                            2022
+                            &nbsp;
+                        </td>
+
+
+
+                        <td className="border border-black" width="15%" align="center">
+                            -
+                            &nbsp;
+                        </td>
+
+                    </tr><tr className="border">
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            HSC
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Science
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Belkuchi College
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="12.5%" align="center"   >
+                            CGPA:3.75<br />out of 5
+                            &nbsp;
+                        </td>
+
+
+                        <td className="border border-black" width="12.5%" align="center">
+                            2018
+                            &nbsp;
+                        </td>
+
+
+
+                        <td className="border border-black" width="15%" align="center">
+                            2016-2017
+                            &nbsp;
+                        </td>
+
+                    </tr><tr className="border">
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            SSC
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Science
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Sohagpur S. K. Pilot Model High School
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="12.5%" align="center"   >
+                            CGPA:4.89<br />out of 5
+                            &nbsp;
+                        </td>
+
+
+                        <td className="border border-black" width="12.5%" align="center">
+                            2015
+                            &nbsp;
+                        </td>
+
+
+
+                        <td className="border border-black" width="15%" align="center">
+                            2013-2014
+                            &nbsp;
+                        </td>
+
+                    </tr>
+                </tbody>
+            </table>
+            <p className='bg-base-100 underline mt-4 font-bold'>Training and Course Summary:</p>
+            <table border="0" cellPadding="0" className='mt-2 border border-slate-400 border-collapse table-compact w-full' cellSpacing="0" align="center" width="100%" >
+                <tbody><tr className="border">
+                    <td className="border border-black" width="20%" align="center"  ><strong>Course Title</strong></td>
+                    <td className="border border-black" width="20%" align="center" ><strong>Topic</strong></td>
+                    <td className="border border-black" width="20%" align="center" ><strong>Institute</strong></td>
+                    <td className="border border-black" width="12.5%" align="center" ><strong>Country</strong></td>
+
+                    <td className="border border-black" width="12.5%" align="center" ><strong>Year</strong></td>
+
+
+                    <td className="border border-black" width="15%" align="center"><strong>Duration</strong></td>
+
+                </tr>
+
+                    <tr className="border">
+
+                        <td className="border border-black" width="20%" align="center" >
+                            Learning and Earning Development Project
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center" >
+                            Graphic Design
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Inflack Ltd, PrimeTech Solutions Ltd &amp; Spinoff Studio (Jv)
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="12.5%" align="center"   >
+                            Bangladesh
+                            &nbsp;
+                        </td>
+
+
+                        <td className="border border-black" width="12.5%" align="center">
+                            2020
+                            &nbsp;
+                        </td>
+
+
+
+                        <td className="border border-black" width="15%" align="center">
+                            50 days
+                            &nbsp;
+                        </td>
+
+                    </tr><tr className="border">
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Complete Web Development Course With Jhankar Mahbub
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Web Development
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="20%" align="center"   >
+                            Programming Hero
+                            &nbsp;
+                        </td>
+
+                        <td className="border border-black" width="12.5%" align="center"   >
+                            Bangladesh
+                            &nbsp;
+                        </td>
+
+
+                        <td className="border border-black" width="12.5%" align="center">
+                            2022
+                            &nbsp;
+                        </td>
+
+
+
+                        <td className="border border-black" width="15%" align="center">
+                            6 Months
+                            &nbsp;
+                        </td>
+
+                    </tr>
+                </tbody>
+            </table>
+
+            <p className='bg-base-100 underline mt-4 font-bold'>Skills:</p>
+
         </div>
     );
 };
